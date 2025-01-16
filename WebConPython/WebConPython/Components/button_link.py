72ae9183517fee64feecd3333311a4f47c299914
RubenGBar <ruben.garcia@iesnervion.es>
2025-01-16 11:15:42 +0100

    Margenes a los botones

--- WebConPython/WebConPython/Components/button_link.py
+++ WebConPython/WebConPython/Components/button_link.py
@@ -3,16 +3,14 @@ import reflex as rx
 from WebConPython.styles.styles import button_title_style, button_body_style
 
 
-def button_link(texto1, texto2, url) -> rx.Component:
+def button_link(texto1, url) -> rx.Component:
     return rx.button(
         rx.link(
-            rx.vstack(
-              rx.text(texto1, style= button_title_style),
-                rx.text(texto2, style= button_body_style)
-            ),
+            rx.text(texto1, style=button_title_style),
             href=url,
             is_external=True,
-            color_scheme="mint"
+            color_scheme="mint",
+            margin = 10
         ),
         color_scheme="tomato",
     )

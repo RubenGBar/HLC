bed154cd2c46cdebad29abc95de0774a6e0d94da
RubenGBar <ruben.garcia@iesnervion.es>
2025-02-12 14:26:24 +0100

    Ejercicio 1 terminado

--- WebSelenium/WebSelenium/WebSelenium.py
+++ WebSelenium/WebSelenium/WebSelenium.py
@@ -4,6 +4,8 @@ import reflex as rx
 from rxconfig import config
 
 from WebSelenium.pages.Index import index
+from WebSelenium.pages.Buscadores import buscadores
+from WebSelenium.pages.RedesSociales import redesSociales
 
 class State(rx.State):
     pass
--- WebSelenium/WebSelenium/pages/Index.py
+++ WebSelenium/WebSelenium/pages/Index.py
@@ -1,12 +1,20 @@
 import reflex as rx
 
 from WebSelenium.Routes import Routes
-from WebSelenium.views.Headers.Header import header
+from WebSelenium.views.Headers.Header import headerIndex
+from WebSelenium.views.Listas.List import listaEnlacesFavoritos
+
 
 @rx.page(
     route=Routes.INDEX.value,
 )
 def index() -> rx.Component:
     return rx.center(
-        header(),
+        rx.vstack(
+            headerIndex(),
+            listaEnlacesFavoritos(),
+            align="center",
+            max_width= "600px",
+            margin_y= "32px",
+        )
     )
